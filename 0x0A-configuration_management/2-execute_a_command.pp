3bcac53e93582a237d3b144c44059e59b5fa9d98
stewie-gil <gilliganstewie7@gmail.com>
2023-03-24 14:26:19 -0700

    lint compliant

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #Kills the killme now process
 
-exec {'killmenow':
-command => 'pkill killmenow',
-path => '/usr/local/bin/:/usr/bin:/bin/',
+exec { 'pkill':
+  command  => 'pkill killmenow',
+  provider => 'shell',
 }
