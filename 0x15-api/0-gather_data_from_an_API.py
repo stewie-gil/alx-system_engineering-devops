221c07e176030ff431c3ffcd07c33767428185a3
stewie-gil <gilliganstewie7@gmail.com>
2023-04-25 02:47:36 -0700

    code style

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,21 +3,21 @@
 Python script to print out todo list info
 """
 
-import requests 
-import sys 
+import requests
+import sys
 
 if __name__ == "__main__":
     employee_id = sys.argv[1]
 
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    usr_url = "https://jsonplaceholder.typicode.com/users/{}"
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos"
+    user1 = user_url.format(employee_id)
+    todo1 = todo_url.format(employee_id)
 
-    user = requests.get(user_url).json()
-    todo_list = requests.get(todo_url).json()
-
+    user = requests.get(user1).json()
+    todo_list = requests.get(todo1).json()
     completed = 0
-    total = 0 
-
+    total = 0
     completed_list = []
 
     for task in todo_list:
@@ -26,9 +26,8 @@ if __name__ == "__main__":
             completed += 1
             completed_list.append(task.get("title"))
 
-    printing= "Employee {} is done with tasks({}/{}):"
+    printing = "Employee {} is done with tasks({}/{}):"
 
     print(printing.format(user.get("name"), completed, total))
     for task in completed_list:
         print("\t {}".format(task))
-
