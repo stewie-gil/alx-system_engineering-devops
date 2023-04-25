0a8dd1b10166e217c7b23dbb369bd08170c0a1bd
stewie-gil <gilliganstewie7@gmail.com>
2023-04-25 04:49:48 -0700

    json format

---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Python script to export user's task list in JSON format
+"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    employee_id = sys.argv[1]
+
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+
+    user = requests.get(user_url).json()
+    todo_list = requests.get(todo_url).json()
+
+    with open('{}.json'.format(employee_id), 'w') as json_file:
+        tasks = []
+        for task in todo_list:
+            tasks.append({
+                "task": task.get("title"),
+                "completed": task.get("completed"),
+                "username": user.get("username")
+            })
+        data = {"{}".format(employee_id): tasks}
+        json.dump(data, json_file)
