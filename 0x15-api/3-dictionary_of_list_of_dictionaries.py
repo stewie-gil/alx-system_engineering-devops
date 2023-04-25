d85b67a648269f8cdaaac64b0e351c1fcd536883
stewie-gil <gilliganstewie7@gmail.com>
2023-04-25 04:51:20 -0700

    m

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -8,10 +8,10 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    employee_id = sys.argv[1]
+    d = sys.argv[1]
 
-    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(employee_id)
+    user_url = "https://jsonplaceholder.typicode.com/users/{}".format(d)
+    todo_url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(d)
 
     user = requests.get(user_url).json()
     todo_list = requests.get(todo_url).json()
