c7ca9f4afb485cc472e2120f889f630f628f4935
stewie-gil <gilliganstewie7@gmail.com>
2023-05-22 05:06:42 -0700

    if name == main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,31 +1,31 @@
 #!/usr/bin/python3
-"""a python script to gather data from an api """
+"""
+A python script to gather data from an api 
+"""
+
+
 import os
 import requests
 import sys
 
-user_id = int(sys.argv[1])
-completed_tasks = 0
-total_tasks = 0
+if __name__ == "__main__":
+    user_id = int(sys.argv[1])
+    completed_tasks = 0
+    total_tasks = 0
+    response = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(user_id))
+    user_data = response.json()
+    name = user_data['name']
+    todos = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id))
+    todo_list = todos.json()
+    for task in todo_list:
+        if task['completed'] is True:
+            completed_tasks = completed_tasks + 1
+        total_tasks = total_tasks + 1
 
-response = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}".format(user_id))
-user_data = response.json()
-
-name = user_data['name']
-
-todos = requests.get(
-    "https://jsonplaceholder.typicode.com/users/{}/todos".format(user_id))
-
-todo_list = todos.json()
-
-for task in todo_list:
-    if task['completed'] is True:
-        completed_tasks = completed_tasks + 1
-    total_tasks = total_tasks + 1
-
-print("Employee {} is done with tasks({}/{})".format(
-    name, completed_tasks, total_tasks))
-for task in todo_list:
-    if task['completed'] is True:
-        print("\t {}".format(task['title']))
+    print("Employee {} is done with tasks({}/{})".format(
+        name, completed_tasks, total_tasks))
+    for task in todo_list:
+        if task['completed'] is True:
+            print("\t {}".format(task['title']))
